839c15fbb157caad23adf8436c9558c65bb20e1b
shikoliamos31 <amosshikoli@gmail.com>
2024-08-09 09:28:14 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,45 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+// Get the Movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Construct the URL to fetch the movie data from the Star Wars API
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+// Make the HTTP GET request to the Star Wars API
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Failed to get data. Status code:', response.statusCode);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+
+  // Fetch all character data in parallel, but print them in the correct order
+  const characterPromises = characters.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (charError, charResponse, charBody) => {
+        if (charError) {
+          reject(charError);
+        } else if (charResponse.statusCode !== 200) {
+          reject(new Error('Failed to get character data'));
+        } else {
+          resolve(JSON.parse(charBody).name);
+        }
+      });
+    });
   });
-};
+
+  // Wait for all promises to resolve and then print the character names in order
+  Promise.all(characterPromises)
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(error => console.error('Error:', error));
+});
